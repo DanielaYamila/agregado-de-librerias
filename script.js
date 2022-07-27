f7f2b981df9f5b6bd646426ccb25f6290592ab18
Daniela Gomez <danielagomezyamila@gmail.com>
2022-07-27 15:27:53 -0300

    add changes II

--- script.js
+++ script.js
@@ -4,22 +4,49 @@ let nombre, apellido;
 do {
     nombre = prompt("Bienvenido, ingrese su nombre").toLowerCase();
     apellido = prompt("Ingrese su apellido").toLowerCase();
-} while (parseFloat(nombre) && (apellido));
+} while (parseFloat(nombre) && parseFloat(apellido));
 
 console.log(`${saludo} ${nombre} ${apellido}!`);
 
-let number1;
-let number2 = 150;
+let respuesta;
 
 do {
-    number1 = parseFloat(prompt("Ingrese un valor en dolares para pasarlo a pesos argentinos."));
-} while (isNaN(number1))
+    let banca, number1, number2;
 
-alert(`El numero ingresado es ${number1}`);
-console.log(number1 * number2);
-alert(`El resultado es ${number1 * number2}`);
-let confirmacion = prompt("¿Desea ingresar mas valores?").toLowerCase();
+    do {
+        number1 = parseFloat(prompt("Ingrese el valor de su producto."));
+        number2 = parseFloat(prompt("Ingrese el valor del envio."));
+        banca = prompt("Ingrese el nombre de su Banco.").toLowerCase();
+        
+        if(isNaN(number2) || isNaN(number2)) {
+            console.log("Los valores no son validos.");
+        }
+    }while (isNaN(number1) || isNaN(number2));
+    
+    switch(banca) {
+    case "provincia":
+        console.log(number1 + number2 / 50)
+        alert(`El resultado es ${number1 + number2 / 15}`)
+        break
+    case "nacion":
+        console.log(number1 + number2 / 50)
+        alert(`El resultado es ${number1 + number2 / 10}`)
+        break
+    case "bbvba":
+        console.log(number1 + number2 / 50)
+        alert(`El resultado es ${number1 + number2 / 20}`)
+        break
+    case "santander rio":
+        console.log(number1 + number2 / 50)
+        alert(`El resultado es ${number1 + number2 / 5}`)
+        break
+    case "itau":
+        console.log(number1 + number2 / 50)
+        alert(`El resultado es ${number1 + number2 / 2}`)
+        break
+    default:
+        console.log("Operacion no valida.")
+    }
 
-if (confirmacion == "no") {
-    repetir = false;
-}
+    respuesta = prompt("¿Desea ingresar mas valores?").toLowerCase();
+} while(respuesta != "no");
