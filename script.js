5c757e95a625b974dd4447a588252f81d6140896
Daniela Gomez <danielagomezyamila@gmail.com>
2022-07-30 18:25:13 -0300

    add function II

--- script.js
+++ script.js
@@ -1,60 +1,24 @@
-let nombre, apellido;
-
-do {
-    nombre = prompt("Bienvenido, ingrese su nombre").toLowerCase();
-    apellido = prompt("Ingrese su apellido").toLowerCase();
-} while (parseFloat(nombre) && parseFloat(apellido));
-
-console.log(`¡Hola ${nombre} ${apellido}!`);
-
 let respuesta;
 
 do {
-    function veResultados(num1, num2, bank) {
-        console.log(num1);
-        console.log(num2);
-        console.log(bank);
+    function veResultados(peso, altura) {
+        console.log(peso / (altura * altura));
     }
 
-    let banca, number1, number2;
+    let peso, altura;
 
     do {
-        number1 = parseFloat(prompt("Ingrese el valor de su producto."));
-        number2 = parseFloat(prompt("Ingrese el valor del envio."));
-        banca = prompt("Ingrese el nombre de su Banco.").toLowerCase();
-        
-        if(isNaN(number2) || isNaN(number2)) {
-            console.log("Los valores no son validos.");
-        }
-    }while (isNaN(number1) || isNaN(number2));
-
-    veResultados(number1, number2, banca);
-    
-    switch(banca) {
-    case "provincia":
-        console.log(number1 + number2 / 6);
-        alert(`El valor total de la compra es ${number1 + number2 / 6}`);
-        break;
-    case "nacion":
-        console.log(number1 + number2 / 4);
-        alert(`El valor total de la compra es ${number1 + number2 / 4}`);
-        break;
-    case "bbvba":
-        console.log(number1 + number2 / 5);
-        alert(`El valor total de la compra es ${number1 + number2 / 5}`);
-        break;
-    case "santander rio":
-        console.log(number1 + number2 / 3);
-        alert(`El valor total de la compra es ${number1 + number2 / 3}`);
-        break;
-    case "itau":
-        console.log(number1 + number2 / 2);
-        alert(`El valor total de la compra es ${number1 + number2 / 2}`);
-        break;
-    default:
-        console.log("Operacion no valida.");
+        peso = parseFloat(prompt("Ingrese su peso."));
+        altura = parseInt(prompt("Ingrese su altura."));
+    } while (isNaN(peso) || isNaN(altura));
+       
+    if(isNaN(peso) || isNaN(altura)) {
+        console.log("Los valores no son validos.");
     }
-
-    respuesta = prompt("¿Desea ingresar mas valores?").toLowerCase();
+    
+    veResultados(peso, altura);
+    alert(`Su indice de masa corporal es ` + peso / (altura * altura));
+    
+    respuesta = prompt("¿Desea calcula de nuevo su IMC?").toLowerCase();
     
 } while(respuesta != "no");
